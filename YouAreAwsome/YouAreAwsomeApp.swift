7400fce43e3dd5e1ad51698ea67c1e9ca7208e2c
Mohammed <mohammedemad557@yahoo.com>
2026-01-14 21:28:19 +0200

    Initial Commit

---
+++ YouAreAwsome/YouAreAwsomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwsomeApp.swift
+//  YouAreAwsome
+//
+//  Created by Mohammed on 14/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwsomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
