d00e643c91eb2692f28d06327022f799317021ef
Mohammed <mohammedemad557@yahoo.com>
2026-01-15 14:37:27 +0200

    after first lesson

--- YouAreAwsome/ContentView.swift
+++ YouAreAwsome/ContentView.swift
@@ -9,13 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+        Text("What is The Football to You?")
+                .font(.largeTitle)
+                .fontWeight(.thin)
+                .foregroundStyle(.green)
+            HStack{
+                Image(systemName:"figure.american.football")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.blue)
+                Image(systemName:"figure.australian.football")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.indigo)
+                Image(systemName:"figure.indoor.soccer")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.purple)
+            }
+            
         }
-        .padding()
+//        VStack {
+//            Image(systemName: "swift")
+//                .resizable()
+//                .scaledToFit()
+//                .foregroundStyle(.orange)
+//            Text("You, Are Awsome!")
+//                .font(.largeTitle)
+//                .fontWeight(.heavy)
+//                .foregroundStyle(.red)
+//        }
     }
 }
 
