c73ed0df356e53b4bafb21786dd62b2391685d7e
Mohammed <mohammedemad557@yahoo.com>
2026-01-15 14:39:37 +0200

    new

--- YouAreAwsome/ContentView.swift
+++ YouAreAwsome/ContentView.swift
@@ -27,6 +27,7 @@ struct ContentView: View {
                     .resizable()
                     .scaledToFit()
                     .foregroundStyle(.purple)
+                
             }
             
         }
