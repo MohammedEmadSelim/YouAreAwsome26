75955e6193242cb9fb21065be2c58c29c689806a
Mohammed <mohammedemad557@yahoo.com>
2026-01-21 23:12:36 +0200

    state key word  and imutable

--- YouAreAwsome/ContentView.swift
+++ YouAreAwsome/ContentView.swift
@@ -8,39 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var message = " i am a developer"
     var body: some View {
-        VStack{
-        Text("What is The Football to You?")
+//        VStack{
+//        Text("What is The Football to You?")
+//                .font(.largeTitle)
+//                .fontWeight(.thin)
+//                .foregroundStyle(.green)
+//            HStack{
+//                Image(systemName:"figure.american.football")
+//                    .resizable()
+//                    .scaledToFit()
+//                    .foregroundStyle(.blue)
+//                Image(systemName:"figure.australian.football")
+//                    .resizable()
+//                    .scaledToFit()
+//                    .foregroundStyle(.indigo)
+//                Image(systemName:"figure.indoor.soccer")
+//                    .resizable()
+//                    .scaledToFit()
+//                    .foregroundStyle(.purple)
+//                
+//            }
+//            
+//        }
+        VStack {
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+            Text(message)
                 .font(.largeTitle)
-                .fontWeight(.thin)
-                .foregroundStyle(.green)
-            HStack{
-                Image(systemName:"figure.american.football")
-                    .resizable()
-                    .scaledToFit()
-                    .foregroundStyle(.blue)
-                Image(systemName:"figure.australian.football")
-                    .resizable()
-                    .scaledToFit()
-                    .foregroundStyle(.indigo)
-                Image(systemName:"figure.indoor.soccer")
-                    .resizable()
-                    .scaledToFit()
-                    .foregroundStyle(.purple)
-                
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+            Button(message) {
+                    message = "Goutch"
+                print("Goutch")
             }
             
         }
-//        VStack {
-//            Image(systemName: "swift")
-//                .resizable()
-//                .scaledToFit()
-//                .foregroundStyle(.orange)
-//            Text("You, Are Awsome!")
-//                .font(.largeTitle)
-//                .fontWeight(.heavy)
-//                .foregroundStyle(.red)
-//        }
     }
 }
 
