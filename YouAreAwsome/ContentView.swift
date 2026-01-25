cc44bbb884d4828a70f3e5c1ceed0a36e49ec319
Mohammed <mohammedemad557@yahoo.com>
2026-01-25 21:37:20 +0200

    after lesson 10 1.3

--- YouAreAwsome/ContentView.swift
+++ YouAreAwsome/ContentView.swift
@@ -9,44 +9,59 @@ import SwiftUI
 
 struct ContentView: View {
     @State private  var message = " i am a developer"
+    
     var body: some View {
-//        VStack{
-//        Text("What is The Football to You?")
-//                .font(.largeTitle)
-//                .fontWeight(.thin)
-//                .foregroundStyle(.green)
-//            HStack{
-//                Image(systemName:"figure.american.football")
-//                    .resizable()
-//                    .scaledToFit()
-//                    .foregroundStyle(.blue)
-//                Image(systemName:"figure.australian.football")
-//                    .resizable()
-//                    .scaledToFit()
-//                    .foregroundStyle(.indigo)
-//                Image(systemName:"figure.indoor.soccer")
-//                    .resizable()
-//                    .scaledToFit()
-//                    .foregroundStyle(.purple)
-//                
-//            }
-//            
-//        }
-     
+        //        VStack{
+        //        Text("What is The Football to You?")
+        //                .font(.largeTitle)
+        //                .fontWeight(.thin)
+        //                .foregroundStyle(.green)
+        //            HStack{
+        //                Image(systemName:"figure.american.football")
+        //                    .resizable()
+        //                    .scaledToFit()
+        //                    .foregroundStyle(.blue)
+        //                Image(systemName:"figure.australian.football")
+        //                    .resizable()
+        //                    .scaledToFit()
+        //                    .foregroundStyle(.indigo)
+        //                Image(systemName:"figure.indoor.soccer")
+        //                    .resizable()
+        //                    .scaledToFit()
+        //                    .foregroundStyle(.purple)
+        //
+        //            }
+        //
+        //        }
+        
         
         VStack {
+            Spacer()
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
+                .frame(width: 200, height: 200)
             Text(message)
                 .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
-            Button("Click me") {
-                    message = "Goutch"
-                print("Goutch")
+                .fontWeight(.ultraLight)
+                .foregroundStyle(.black)
+            Spacer()
+            HStack{
+                Button("Aweosme!") {
+                    message = "Aweosme"
+                    print("Aweosme")
+                }
+                Button("Great!"){
+                    message = "Great"
+                    print("Great")
+                }
+                
+                
             }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
             
         }
     }
